8e1d42f3472d93f8b6b921f9acef74ccd9a46094
098ff <chanatdakc@gmail.com>
2025-04-15 20:39:18 +0700

    ExecutionPath: update SEAT.csv data & Add qry ofr expl

---
+++ exec-path/qry2.sql
@@ -0,0 +1,23 @@
+(SELECT 
+  t.TicketID, 
+  t.PassengerName, 
+  t.SeatNo, 
+  t.Status, 
+  f.FlightNo, 
+  f.Schedule 
+FROM TICKET t 
+JOIN SEAT s ON t.SeatNo = s.SeatNo 
+JOIN FLIGHT f ON s.FlightNo = f.FlightNo
+WHERE f.FlightNo = 'F101')
+UNION
+(SELECT 
+  t.TicketID, 
+  t.PassengerName, 
+  t.SeatNo, 
+  t.Status, 
+  f.FlightNo, 
+  f.Schedule 
+FROM TICKET t 
+JOIN SEAT s ON t.SeatNo = s.SeatNo 
+JOIN FLIGHT f ON s.FlightNo = f.FlightNo
+WHERE f.FlightNo = 'F102');
