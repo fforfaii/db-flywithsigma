ff2927c69a7fddd58df5d62d48bfc3de84339358
Mysterioucz <chatrinyoonchalard@gmail.com>
2025-04-18 19:17:33 +0700

    updated

--- stored-proc/MakePayment.sql
+++ stored-proc/MakePayment.sql
@@ -13,35 +13,6 @@ DECLARE
     expectedAmount DECIMAL(10,2);
     currentStatus VARCHAR(20);
 BEGIN
-    -- ดึง PaymentID จาก PURCHASE
-    SELECT PaymentID INTO existingPaymentID
-    FROM PURCHASE
-    WHERE UserID = p_UserID AND TicketID = p_TicketID;
-
-    IF existingPaymentID IS NULL THEN
-        RAISE EXCEPTION 'Payment record not found for this User and Ticket.';
-    END IF;
-
-    -- ดึงสถานะปัจจุบันของ Payment
-    SELECT TransactionStatus INTO currentStatus
-    FROM PAYMENT
-    WHERE PaymentID = existingPaymentID;
-
-    -- ตรวจสอบว่าจ่ายเงินไปแล้วหรือยัง
-    IF currentStatus = 'Success' THEN
-        RAISE EXCEPTION 'Payment has already been completed.';
-    END IF;
-
-    -- ดึงราคาที่ต้องจ่ายจริงจาก TICKET
-    SELECT Price INTO expectedAmount
-    FROM TICKET
-    WHERE TicketID = p_TicketID;
-
-    -- ตรวจสอบว่าจ่ายเงินถูกต้องหรือไม่
-    IF expectedAmount IS DISTINCT FROM p_Amount THEN
-        RAISE EXCEPTION 'Invalid payment amount: expected %, got %.', expectedAmount, p_Amount;
-    END IF;
-
     -- อัปเดตข้อมูลใน PAYMENT
     UPDATE PAYMENT
     SET
--- trigger-function/trg_user_purchase.sql
+++ trigger-function/trg_user_purchase.sql
@@ -2,23 +2,38 @@ CREATE OR REPLACE FUNCTION trg_user_purchase()
 RETURNS TRIGGER
 LANGUAGE plpgsql
 AS $$
-DECLARE verify BOOLEAN;
+DECLARE 
+    verify BOOLEAN;
+    existingPaymentID VARCHAR(10);
+    expectedAmount DECIMAL(10,2);
+    currentStatus VARCHAR(20);
 BEGIN
+    -- Check User Verification
     SELECT u.VerificationStatus INTO verify
     FROM app_user u
     WHERE NEW.UserAccountID = u.AccountID;
-
+    
     IF NOT verify THEN
         RAISE EXCEPTION 'User is not verified';
-    ELSE
-		UPDATE payment
-		SET transactionstatus = 'Success'
-		WHERE NEW.paymentid = paymentid;
+    END IF;
 
-		UPDATE TICKET
-		SET TicketStatus = 'Confirmed' -- Need Recheck
-		WHERE NEW.ticketid = ticketid;
-	END IF;
+    -- Check does payment record exist
+    SELECT PaymentID INTO existingPaymentID
+    FROM PURCHASE
+    WHERE ticketid = NEW.ticketid AND PaymentID = NEW.paymentid;
+
+    IF existingPaymentID IS NULL THEN
+        RAISE EXCEPTION 'Payment record not found for this User and Ticket.';
+    END IF;
+
+    -- Check that if payment already paid
+    SELECT TransactionStatus INTO currentStatus
+    FROM PAYMENT
+    WHERE PaymentID = NEW.paymentid;
+
+    IF currentStatus = 'Success' THEN
+        RAISE EXCEPTION 'Payment has already been completed.';
+    END IF;
 
     RETURN NEW;
 END;
